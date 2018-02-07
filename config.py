4e20868bc43c3db4033ba7a78c6a242d89a4bddc
Mitchell Kossoris <mkossoris@gmail.com>
2018-02-06 23:00:19 -0600

    Created heart rate normalizer

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 class Config:
     def __init__(self):
-        self.localPath = "/Users/scholl/Desktop/IndependentStudyZheng"
-        self.fileNames = []
+        self.localPath = "/Users/NA/Desktop/Independent Study/Data/"
+        self.fileNames = ["T001.csv"]
 
-        for x in range(1,10):
-            self.fileNames.append("T00" + str(x) + ".csv")
-        for x in range (10, 89):
-            self.fileNames.append("T0" + str(x) + ".csv")
+        # for x in range(1,10):
+        #     self.fileNames.append("T00" + str(x) + ".csv")
+        # for x in range (10, 89):
+        #     self.fileNames.append("T0" + str(x) + ".csv")
 
         self.columnNames = ["Time", "Drive",	"Stimulus", "Failure", "Palm.EDA", "Heart.Rate", "Breathing.Rate",
                "Perinasal.Perspiration", "Speed", "Acceleration", "Brake", "Steering", "LaneOffset",
--- normalizer.py
+++ normalizer.py
@@ -1,24 +1,54 @@
 import csv
 import config
+import sys
+
+def heartRateNormalizer(data):
+    minHR = float(sys.maxsize)
+    maxHR = float(-1)
+
+    for hr in data:
+        try:
+            hr = float(hr)
+            if hr != 0:
+                if hr < minHR:
+                    minHR = float(hr)
+                if hr > maxHR:
+                    maxHR = float(hr)
+        except ValueError:
+            pass
+
+    for i in range(len(data)):
+        try:
+            data[i] = float(data[i])
+            if data[i] == 0:
+                data[i] = -1
+            else:
+                data[i] = (data[i] - minHR) / (maxHR - minHR)
+        except ValueError:
+            data[i] = -1
+
+    return data
+
 
 configs = config.Config()
-
-print(configs.fileNames)
-
 
 for file in configs.fileNames:
     file = configs.localPath + file
 
-    columns = {}
+    columnData = {}
 
     for columnName in configs.columnNames:
-        columns[columnName] = []
+        columnData[columnName] = []
 
     dictReader = csv.DictReader(open(file, 'rt'), fieldnames=configs.columnNames,
                                 delimiter=',', quotechar='"')
 
     for row in dictReader:
         for key in row:
-            columns[key].append(row[key])
+            columnData[key].append(row[key])
+    print(columnData["Heart.Rate"])
 
+    columnData["Heart.Rate"] = heartRateNormalizer(columnData["Heart.Rate"])
+
+    print(columnData["Heart.Rate"])
 
