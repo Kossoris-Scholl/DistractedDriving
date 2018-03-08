e780179a98ef9ab84d33f802fd5b6775e574a1c0
cjscholl <catelynjscholl@gmail.com>
2018-03-08 15:33:12 -0600

    Made changes to KNN algorithm and rolling mean function

--- Analysis/knn.py
+++ Analysis/knn.py
@@ -13,24 +13,24 @@ import numpy as np
 configs = config.Config()
 path = configs.localPathAverage
 df = pd.concat((pd.read_csv(f) for f in glob.glob(os.path.join(path, '*.csv'))))
-df = df.fillna(method='ffill')
+df.to_csv("concat.csv", sep=',', index=False)
 
-X = df['Breathing.Rate']
-y = df['Stimulus']
+X = df['Speed']
+y = df['Stimulus'].replace([2,3,4,5,6], 1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-X_train = X_train.drop('Breathing.Rate').values.reshape(-1,1)
-y_train = y_train.values
+X_train = X_train.drop('Speed').values.reshape(-1,1)
+y_train = y_train.values.astype('int')
 
-## Instantiate the model with 5 neighbors.
+## Instantiate the model with 50 neighbors.
 knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=50)
 
 ## Fit the model on the training data.
 knn.fit(X_train, y_train)
 
 ## See how the model performs on the test data.
-print(knn.score(X_test.values.reshape(-1,1), y_test.values))
+print(knn.score(X_test.values.reshape(-1,1), y_test.values.astype('int')))
 
 ## Test the model & return calculate mean square error
 predictions = knn.predict(X_test.values.reshape(-1,   1))
